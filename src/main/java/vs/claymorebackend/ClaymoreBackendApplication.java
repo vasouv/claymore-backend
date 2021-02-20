e8a4f419da17c59938fbbb94b186e6a66824e591
Vasilis Souvatzis <vasouv@gmail.com>
2021-02-20 21:42:08 +0200

    Initial commit

---
+++ src/main/java/vs/claymorebackend/ClaymoreBackendApplication.java
@@ -0,0 +1,13 @@
+package vs.claymorebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClaymoreBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClaymoreBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/vs/claymorebackend/ClaymoreBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package vs.claymorebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClaymoreBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
